cef63e32247114456d98f3cc7a3a787dfe65a2d4
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-03-02 17:11:58 +0600

    all are completed

---
+++ script/main_script.js
@@ -0,0 +1,72 @@
+// Initialize the total number of tasks
+const assTask=document.getElementById('task')
+let totalTasks = parseInt(assTask.innerText); 
+
+function ifAllTaskComplete() {
+    if (totalTasks === 0) {
+        alert("You have successfully completed all tasks!");
+    }
+}
+
+// selection
+function selection(buttonId, titleId) {
+    alert('Board Updated Successfully');
+    document.getElementById(buttonId).disabled = true;
+    document.getElementById(buttonId).style.backgroundColor = '#B1BCFF'; 
+
+    const notify = document.getElementById('notification');
+    const title = document.getElementById(titleId);
+    const newTitle = title.innerText;
+    const notityElement = document.createElement('p');
+
+    const time = new Date();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+
+    const word = 'jahin';
+    const result = word + " " + newTitle + " " + (`${hours}:${minutes}`);
+
+    const countTask = document.getElementById('total_task');
+    const count = parseInt(countTask.innerText);
+    const total = count + 1;
+    countTask.textContent = total;
+
+    const assTask = document.getElementById('task');
+    const assTn = parseInt(assTask.innerText);
+    const assTotal = assTn - 1;
+    assTask.textContent = assTotal;
+
+    notityElement.innerText = result;
+    notify.appendChild(notityElement);
+
+    // Decrement the total task count
+    totalTasks--;
+
+    // Check if all tasks are completed
+    ifAllTaskComplete();
+}
+
+// call all the btn-id
+document.getElementById('btn-selection-1').addEventListener('click', function () {
+    selection('btn-selection-1', 'Sec-title-1');
+});
+
+document.getElementById('btn-selection-2').addEventListener('click', function () {
+    selection('btn-selection-2', 'Sec-title-2');
+});
+
+document.getElementById('btn-selection-3').addEventListener('click', function () {
+    selection('btn-selection-3', 'Sec-title-3');
+});
+
+document.getElementById('btn-selection-4').addEventListener('click', function () {
+    selection('btn-selection-4', 'Sec-title-4');
+});
+
+document.getElementById('btn-selection-5').addEventListener('click', function () {
+    selection('btn-selection-5', 'Sec-title-5');
+});
+
+document.getElementById('btn-selection-6').addEventListener('click', function () {
+    selection('btn-selection-6', 'Sec-title-6');
+});
