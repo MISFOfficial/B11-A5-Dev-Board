e28766cccb61bf30da75b8eb9f271fd6e13d2894
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-03-02 13:35:40 +0600

    delete funtion complete

---
+++ script/delete.js
@@ -0,0 +1,5 @@
+document.getElementById('delete').addEventListener('click', function(){
+    const element = document.getElementById('notification');
+    // console.log(element)
+    element.innerText=''
+})
